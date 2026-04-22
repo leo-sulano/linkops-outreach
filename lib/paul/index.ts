e20ddfabe24a94962e8e4db47605d964725dd34c
Leo <leo@optinetsolutions.com>
2026-04-22 20:31:38 +0800

    feat: add Paul module exports

---
+++ lib/paul/index.ts
@@ -0,0 +1,5 @@
+export { qualifyDomain } from './qualifier';
+export type { DomainScore, QualifyInput, DomainFactors } from './types';
+
+export { generateOutreach } from './generator';
+export type { OutreachGeneratorInput, OutreachGeneratorOutput } from './types';
